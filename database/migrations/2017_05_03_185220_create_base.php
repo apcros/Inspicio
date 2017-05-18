1e06a75f19c2350eb7eb2cdfc1cbe3464b246f8c
Jules Decol <apcros.mail@gmail.com>
2017-05-18 23:08:06 +0100

    Added db seeder

--- database/migrations/2017_05_03_185220_create_base.php
+++ database/migrations/2017_05_03_185220_create_base.php
@@ -1,94 +1,93 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateBase extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('nickname');
-            $table->integer('rank');
-            $table->integer('points');
-            $table->timestamps();
-            $table->primary('id');
-        });
+class CreateBase extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('users', function (Blueprint $table) {
+			$table->uuid('id');
+			$table->string('name');
+			$table->string('email')->unique();
+			$table->string('nickname');
+			$table->integer('rank');
+			$table->integer('points');
+			$table->timestamps();
+			$table->primary('id');
+		});
 
-        Schema::create('accounts', function (Blueprint $table) {
-            $table->uuid('id');
-            $table->string('login');
-            $table->uuid('user_id');
-            $table->string('token');
-            $table->string('provider');
-            $table->boolean('is_main');
-            $table->unique(array('login','provider'));
-            $table->timestamps();
+		Schema::create('accounts', function (Blueprint $table) {
+			$table->uuid('id');
+			$table->string('login');
+			$table->uuid('user_id');
+			$table->string('token');
+			$table->string('provider');
+			$table->boolean('is_main');
+			$table->unique(array('login', 'provider'));
+			$table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->primary('id');
-        });
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->primary('id');
+		});
 
-        Schema::create('skills', function (Blueprint $table) {
-            $table->uuid('id');
-            $table->string('name');
-            $table->uuid('user_id');
-            $table->boolean('is_verified');
-            $table->integer('level');
-            $table->timestamps();
+		Schema::create('skills', function (Blueprint $table) {
+			$table->uuid('id');
+			$table->string('name');
+			$table->uuid('user_id');
+			$table->boolean('is_verified');
+			$table->integer('level');
+			$table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->primary('id');
-        });
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->primary('id');
+		});
 
-        Schema::create('requests', function (Blueprint $table) {
-            $table->uuid('id');
-            $table->string('name');
-            $table->text('description');
-            $table->string('url');
-            $table->string('status');
-            $table->string('language');
-            $table->string('repository');
-            $table->uuid('account_id');
-            $table->uuid('author_id');
-            $table->timestamps();
+		Schema::create('requests', function (Blueprint $table) {
+			$table->uuid('id');
+			$table->string('name');
+			$table->text('description');
+			$table->string('url');
+			$table->string('status');
+			$table->string('language');
+			$table->string('repository');
+			$table->uuid('account_id');
+			$table->uuid('author_id');
+			$table->timestamps();
 
-            $table->foreign('author_id')->references('id')->on('users');
-            $table->foreign('account_id')->references('id')->on('accounts');
-            $table->primary('id');
-        });
+			$table->foreign('author_id')->references('id')->on('users');
+			$table->foreign('account_id')->references('id')->on('accounts');
+			$table->primary('id');
+		});
 
-        Schema::create('request_tracking', function (Blueprint $table) {
-            $table->uuid('user_id');
-            $table->uuid('request_id');
-            $table->string('status');
-            $table->timestamps();
+		Schema::create('request_tracking', function (Blueprint $table) {
+			$table->uuid('user_id');
+			$table->uuid('request_id');
+			$table->string('status');
+			$table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('request_id')->references('id')->on('requests');
-            $table->primary(['user_id','request_id']);
-        });
-    }
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->foreign('request_id')->references('id')->on('requests');
+			$table->primary(['user_id', 'request_id']);
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('request_tracking');
-        Schema::dropIfExists('requests');
-        Schema::dropIfExists('skills');
-        Schema::dropIfExists('users');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+
+		Schema::dropIfExists('request_tracking');
+		Schema::dropIfExists('requests');
+		Schema::dropIfExists('skills');
+		Schema::dropIfExists('accounts');
+		Schema::dropIfExists('users');
+	}
 }
