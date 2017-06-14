a5e2d2a2f71d4e63f3a6d10fe0f1601807f489e0
Jules Decol <apcros.mail@gmail.com>
2017-06-14 21:26:59 +0100

    Custom font

--- resources/views/layouts/bootstrap-main.blade.php
+++ resources/views/layouts/bootstrap-main.blade.php
@@ -4,11 +4,13 @@
 	<head>
 		<title>Inspicio - @yield('title')</title>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<link href="https://fonts.googleapis.com/css?family=Molengo" rel="stylesheet">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="{{ asset('css/snackbar.min.css') }}">
 		<link rel="stylesheet" href="{{ asset('css/snackbar-material.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/inspicio.css') }}">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		<script type="text/javascript" src="{{ asset('js/snackbar.min.js') }}"></script>
 		<script type="text/javascript" src="{{ asset('js/popout.js') }}"></script>
