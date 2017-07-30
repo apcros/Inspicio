00dfbab9c21f4222872721cd546b8742aedbee9e
Jules Decol <apcros.mail@gmail.com>
2017-07-30 21:43:48 +0100

    Added migration for Auto Import

---
+++ app/AutoImport.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use App\User;
+use App\ReviewRequest;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class AutoImport {
+
+	private function listAllWatchedRepositories() {
+
+	}
+
+	public function run() {
+		
+	}
+}
