bfdcb5a6e73c8ad874a7df325cb33a2fd0f058bf
Apcros <apcros.mail@gmail.com>
2018-01-02 23:10:34 +0000

    Fixed pagination breaking loading animation on search

--- public/js/search.js
+++ public/js/search.js
@@ -2,8 +2,12 @@ function search(page, trigger_loading = true) {
 	var query_val = $("#review-keywords").val();
 	var languages_selected = $("#review-language").val();
 	var search_in_closed = $("#review-can-be-closed").prop('checked');
+	var button_state;
 
-	var button_state = window.startLoading("#start-search-btn");
+	if(trigger_loading) {
+		button_state = window.startLoading("#start-search-btn");
+	}
+	
 
 	$.post(window.location.origin+"/api/reviews/search",
 		{
@@ -20,13 +24,21 @@ function search(page, trigger_loading = true) {
 
 			if(result.reviews.prev_page_url != null){
 				$( "#previous-a" ).click(function() {
-				  window.search((result.reviews.current_page-1));
+				  var previous_btn_state = window.startLoading("#previous-a");
+				  var btn_state = window.startLoading("#start-search-btn");
+				  window.search((result.reviews.current_page-1),false);
+				  window.stopLoading("#previous-a", previous_btn_state);
+				  window.stopLoading("#start-search-btn",btn_state);
 				});
 			}
 
 			if(result.reviews.next_page_url != null) {
 				$("#next-a").click(function() {
-					window.search((result.reviews.current_page+1));
+					var next_btn_state = window.startLoading("#next-a");
+					var btn_state = window.startLoading("#start-search-btn");
+					window.search((result.reviews.current_page+1),false);
+					window.stopLoading("#next-a", next_btn_state);
+					window.stopLoading("#start-search-btn",btn_state);
 				});
 			}
 
@@ -35,7 +47,9 @@ function search(page, trigger_loading = true) {
 		Materialize.toast("Error while searching. Please try again", 4000, "red");
 	})
 	.always(function() {
-		window.stopLoading("#start-search-btn",button_state);
+		if(trigger_loading) {
+			window.stopLoading("#start-search-btn",button_state);
+		}
 	})
 }
 $(document).ready(function() {
