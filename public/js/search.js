ac08e78337735d4627d411904aab5c698364fb33
Jules Decol <apcros.mail@gmail.com>
2018-01-07 18:12:15 +0000

    Fixed Codacy issues

--- public/js/search.js
+++ public/js/search.js
@@ -1,8 +1,8 @@
 function search(page) {
 	var query_val = $("#review-keywords").val();
 	var languages_selected = $("#review-language").val();
-	var search_in_closed = $("#review-can-be-closed").prop('checked');
-	var button_state = window.startLoading("#start-search-btn");
+	var search_in_closed = $("#review-can-be-closed").prop("checked");
+	var buttonState = window.startLoading("#start-search-btn");
 
 	
 
@@ -17,35 +17,35 @@ function search(page) {
 		}
 		,function(result) {
 
-			var previous_handler = function () {
+			var previousHandler = function () {
 				window.search((result.reviews.current_page-1));
 			};
 
-			var next_handler = function() {
+			var nextHandler = function() {
 				window.search((result.reviews.current_page+1));
 			};
 
 			$("#previous-a").unbind();
 			$("#next-a").unbind();
 
-			window.updateOrCreateVue('searchreviews','#reviews-search-result', 'reviews', result.reviews);
+			window.updateOrCreateVue("searchreviews","#reviews-search-result", "reviews", result.reviews);
 
 			if(result.reviews.prev_page_url != null){
-				$( "#previous-a" ).bind("click", previous_handler);
+				$( "#previous-a" ).bind("click", previousHandler);
 			}
 
 			if(result.reviews.next_page_url != null) {
-				$("#next-a").bind("click", next_handler);
+				$("#next-a").bind("click", nextHandler);
 			}
 
-	},'json')
+	},"json")
 	.fail(function() {
 		Materialize.toast("Error while searching. Please try again", 4000, "red");
 	})
 	.always(function() {
-		window.stopLoading("#start-search-btn",button_state);
+		window.stopLoading("#start-search-btn",buttonState);
 	})
 }
 $(document).ready(function() {
-	$('#review-language').select2({placeholder: "Language(s)"});
+	$("#review-language").select2({placeholder: "Language(s)"});
 })
